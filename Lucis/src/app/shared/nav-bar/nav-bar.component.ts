0feb29e931701adc3bb3a49c4cba094a64af79db
PwGiza <pwgiza@outlook.com>
2025-06-11 17:18:54 +0200

    style test

---
+++ Lucis/src/app/shared/nav-bar/nav-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-nav-bar',
+  imports: [NgbNavModule],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.scss'
+})
+export class NavBarComponent {
+
+}
